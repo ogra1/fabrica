24da519866a5555b9b008b81d430ffba86b6fa52
James Jesudason <james.jesudason@gmail.com>
2020-05-19 09:07:42 +0100

    Pre-build the SCSS files There are too many build issues on Travis and Launchpad with node-sass, so stored the built CSS instead

---
+++ static/precache-manifest.e3f7795a7cc910f0cb31179d7ee8e785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339f4bc4b5101bd650b63077f831813b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb13f90c974861bfbe52",
+    "url": "/static/css/2.dd06715d.chunk.css"
+  },
+  {
+    "revision": "e536d1cc1d2b94ae3cd1",
+    "url": "/static/css/main.3ec4bfda.chunk.css"
+  },
+  {
+    "revision": "cb13f90c974861bfbe52",
+    "url": "/static/js/2.464d1d33.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.464d1d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e536d1cc1d2b94ae3cd1",
+    "url": "/static/js/main.68f145a3.chunk.js"
+  },
+  {
+    "revision": "4d824322bb1d9b5ec98e",
+    "url": "/static/js/runtime-main.c6ec4ccb.js"
+  }
+]);
